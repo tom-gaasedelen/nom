7be2f675868e8bf253d5875975fe1fde3df0ace8
Naomi Jean Wan Kai Chan <nchan@Naomis-MacBook-Pro.local>
2016-09-20 12:50:09 -0500

    here it comes

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 $(function (){
 
 
+  $(window).scroll(function (event) {
+    var y = $(window).scrollTop();
+    console.log('y: ' + y);
+
+    
+});
 
 
 
-
-}) //end jquery init 
+}) //end jquery init
